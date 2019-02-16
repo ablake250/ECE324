53e34bd830a3b9c5ba5a19b5af63cf210bc743d9
Alex <axelblake@hotmail.com>
2019-02-15 16:36:40 -0800

    testing clock signal on HW4

---
+++ Homework4/usrTB.sv
@@ -0,0 +1,31 @@
+`timescale 10ps/1ps
+
+module usrTB;
+    parameter  N_BITS = 4;
+    logic   clk, reset;
+    logic [1:0] opcode;
+    logic [N_BITS-1:0] d, q;
+
+    univ_shift_reg usr0(
+        .clk(clk),
+        .reset(reset),
+        .ctrl(opcode),
+        .d(d), 
+        .q(q)
+
+    );
+
+    always begin
+        #5 clk <= ~clk;
+    end
+
+    initial begin
+        #10;
+        #10;
+
+
+
+        #10 $stop;
+    end
+
+endmodule
