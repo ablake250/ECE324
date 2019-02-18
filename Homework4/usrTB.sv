95c6a699de556cc4f24e679ee36d0260c33b48c5
Alex <axelblake@hotmail.com>
2019-02-18 13:04:52 -0800

    tb 2

--- Homework4/usrTB.sv
+++ Homework4/usrTB.sv
@@ -1,14 +1,12 @@
 `timescale 10ps/1ps
 
-module usrTB;
+module usrTB();
     parameter  N_BITS = 4;      //bits to instantiate the width of the register, specified at 4 bits
     logic   clk = 0, reset;
     logic [1:0] opcode;
     logic [N_BITS-1:0] d, q;
 
-    univ_shift_reg usr0 
-    #(.N(N_BITS))
-    (            //instantiate a univ_shift_reg module
+    univ_shift_reg #(.N(N_BITS)) usr0 (            //instantiate a univ_shift_reg module
         .clk(clk),
         .reset(reset),
         .ctrl(opcode),
@@ -21,11 +19,11 @@ module usrTB;
     end
 
     initial begin
-        #10     d=4'b0011 opcode = 2'b11;       //load value of D into shifter reg
+        #10     d=4'b1010; opcode = 2'b11;       //load value of D into shifter reg
         #10     opcode = 2'b00;                 //no Operations should happen for this cycle
         #40     opcode = 2'b01;                 //shift left 4 times
         #40     opcode = 2'b10;                 //shift right 4 times
-        $42     reset = 1;
+        #42     reset = 1;
         #10 $stop;
     end
 endmodule
