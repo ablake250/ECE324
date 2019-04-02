a988ea0c97c8f4c2e439c6acb69b69292553a02d
Alex <axelblake@hotmail.com>
2019-04-01 18:50:58 -0700

    same

--- BallMaze/BallMotion.sv
+++ BallMaze/BallMotion.sv
@@ -26,9 +26,14 @@ logic ballColummVel; ballRowVel;
 logic [4:0] xAcceleration, YAcceleration;
 logic [4:0] xAccel_stg2, yAccel_stg2;
 logic xAccelPositive, yAccelPositive;
-logic xPos_stg2, yPos_stg2;
+logic xAccelPos_stg2, yAccelPos_stg2;
 
 //Velocity Logic Declarations
+logic [4:0] xVelocity, yVelocity;
+logic [4:0] xVel_stg2, yVel_stg2
+logic xVelPos, yVelPos;
+logic xVelPos_stg2, yVelPos_stg2;
+
 
 localparam START_X = 128;
 localparam START_Y = 188;
@@ -128,7 +133,7 @@ always_ff @(posedge clk108MHz) begin
         end
     end
     xAccel_stg2 <= xAcceleration;
-    xPos_stg2 <= xAccelPositive;
+    xAccelPos_stg2 <= xAccelPositive;
 end
 
 //y-axis Acceleration
@@ -159,7 +164,7 @@ always_ff @(posedge clk108MHz) begin
         end
     end
     yAccel_stg2 <= YAcceleration;
-    yPos_stg2 <= yAccelPositive;
+    yAccelPos_stg2 <= yAccelPositive;
 end
 
 ////////////////////////////////////////////////////////////////////////////////////
@@ -169,13 +174,44 @@ end
 //x-axis Velocity
 always_ff @(posedge clk108MHz) begin
     if (resetPressed) begin
-        
+        xVelocity <= 0;
+        xVelPos <= 1;
     end
+    else if(maxTickY)
+        if (xVelPos==0) begin
+            if((xVelocity <= xAccel_stg2) & (xAccelPos_stg2)) begin
+                xVelocity <= xAccel_stg2 - xVelocity;
+                xVelPos <= 1;
+            end
+            else if (({1'b0, xVelocity + xAccelPos_stg2} >= 4'b1111) & !(xAccelPos_stg2)) begin
+                xVelocity <= 4'b1111;
+            end
+            else if (!xAccelPos_stg2) xVelocity <= xVelocity + xAccel_stg2;
+            else if (xAccelPos_stg2) xVelocity <= xVelocity - xAccel_stg2;
+        end
+        else if (xVelPos == 1) begin
+            if ((xVelocity <= xAccel_stg2) & !(xAccelPos_stg2))
+                xVelocity <= xAccel_stg2 - xVelocity;
+                xVelPos <= 0;
+            end
+             else if (({1'b0, xVelocity + xAccelPos_stg2} >= 4'b1111) & (xAccelPos_stg2)) begin
+                xVelocity <= 4'b1111;
+            end
+            else if (!xAccelPos_stg2) xVelocity <= xVelocity - xAccel_stg2;
+            else if (xAccelPos_stg2) xVelocity <= xVelocity + xAccel_stg2;
+        end
+    end
+
+    xVel_stg2 <= xVelocity;
 end
 
 //y-axis Velocity
 always_ff @(posedge clk108MHz) begin
     if (resetPressed) begin
+        yVelocity <= 0;
+        yVelPos <= 1;
+    end
+    else if(maxTickY)
 
     end
 end
@@ -187,9 +223,9 @@ end
 
 //x-axis Velocity
 always_ff @(posedge clk108MHz) begin
-    ballColumn <= 80;
+    //ballColumn <= 80;
     if (resetPressed) begin
-
+        ballColumn = xVel_stg2;
     end
 
 end
